05887f8502e2d23f050b6772da84a80198d0d860
detonavomek <detonavomek@gmail.com>
2014-09-09 07:18:57 +0300

    Add static

--- apps/main/models.py
+++ apps/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+# class Topic(models.Model):
+# 	owner = models.ForeignKey('auth.User', relation_name='user_topics')
+# 	title = models.CharField(max_length=255)
+# 	date_add = models.DateTimeField(default = datetime.datetime.now())
+
+# class Comment(models.Model):
+# 	user = models.ForeignKey('auth.User', relation_name='user_topics')
+# 	topic = models.ForeignKey(Topic, relation_name='topic_comment')
+# 	q_comment = models.ForeignKey(Topic, relation_name='topic_comment')
+# 	text = models.CharField(max_length=255)
+# 	date_add = models.DateTimeField(default = datetime.datetime.now())
