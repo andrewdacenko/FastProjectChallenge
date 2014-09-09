e4d8d8ef863341cd797decaa79ccf091f25d5f5a
detonavomek <detonavomek@gmail.com>
2014-09-09 07:51:34 +0300

    Add data to authorization

--- source/urls.py
+++ source/urls.py
@@ -8,7 +8,9 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'apps.main.views.index', name='index'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^auth/$', 'apps.guest.views.auth', name='auth'),
+    url(r'^logout/$', 'apps.guest.views.logout', name='logout'),
+    url(r'^register/$', 'apps.guest.views.register_user', name='register_user'),
 
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
