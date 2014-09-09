184f3e1c1a4fed571149bad77ef0a9977aabd351
detonavomek <detonavomek@gmail.com>
2014-09-09 09:33:09 +0300

    html

--- apps/main/views.py
+++ apps/main/views.py
@@ -3,19 +3,11 @@ from datetime import datetime, timedelta
 
 from models import *
 
-# Create your views here.
 def index(request):
-	# topics_on_main_page = 3
-	# data = {
-	# 	'active_topics': Topic.objects.filter(  date_add__gt=datetime.datetime.now() - timedelta(days=1),
-	# 			                                date_add__lt=datetime.datetime.now()
-	# 			                                ).order_by('id')[:topics_on_main_page],
-
-	# 	'vote_topics': Topic.objects.filter(	date_add__gt=datetime.datetime.now() - timedelta(days=2),
-	# 			                            	date_add__lt=datetime.datetime.now() - timedelta(days=1)
-	# 			                            	).order_by('id')[:topics_on_main_page],
-
-	# 	'archive_topics': Topic.objects.filter( date_add__gt=datetime.datetime.now() - timedelta(days=2)
-	# 			                            	).order_by('id')[:topics_on_main_page]
-	# }
 	return render(request, 'index.html', {})
+
+def topic(request, topic_id):
+	return render(request, 'topic.html', {})
+
+def state(request):
+	return render(request, 'state.html', {})
--- source/urls.py
+++ source/urls.py
@@ -8,6 +8,12 @@ admin.autodiscover()
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'apps.main.views.index', name='index'),
+    url(r'^topic/(?P<topic_id>\d+)$', 'apps.main.views.topic'),
+    url(r'^active/$', 'apps.main.views.state'),
+    url(r'^voting/$', 'apps.main.views.state'),
+    url(r'^archive/$', 'apps.main.views.state'),
+
+
     url(r'^auth/$', 'apps.guest.views.auth', name='auth'),
     url(r'^logout/$', 'apps.guest.views.logout', name='logout'),
     url(r'^register/$', 'apps.guest.views.register_user', name='register_user'),
