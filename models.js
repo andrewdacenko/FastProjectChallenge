f9f9be355ffbbc1330c53b1da131fc01369fd8bf
Andrew Dacenko <a.s.dvornik@gmail.com>
2014-09-09 04:31:45 +0300

    Topics

--- models.js
+++ models.js
@@ -1,9 +1,13 @@
 // declare collections
 // this code should be included in both the client and the server
-Topics = new Meteor.Collection("topics");
-Comments = new Meteor.Collection("comments");
-Votes = new Meteor.Collection("votes");
+// Topics = new Meteor.Collection("topics");
+// Comments = new Meteor.Collection("comments");
+// Votes = new Meteor.Collection("votes");
 
-CommentLikes = new Meteor.Collection('comment_likes');
-TopicLikes = new Meteor.Collection('topic_likes');
-VoteLikes = new Meteor.Collection('vote_likes');
+// CommentLikes = new Meteor.Collection('comment_likes');
+// TopicLikes = new Meteor.Collection('topic_likes');
+// VoteLikes = new Meteor.Collection('vote_likes');
+
+// Topics.insert({
+// 	title: 1
+// });
--- server/server.js
+++ server/server.js
@@ -1,3 +1,37 @@
-Meteor.startup(function() {
-	// code to run on server at startup
-});
+// Meteor.publish("activeTopics", function() {
+// 	var now = new Date();
+// 	var yesterday = new Date();
+// 	yesterday.setDate(now.getDate() - 1);
+// 	return Topics.find({}, {
+// 		dateAdd: {
+// 			$gte: yesterday
+// 		}
+// 	});
+// });
+
+// Meteor.publish("voteTopics", function() {
+// 	var now = new Date();
+// 	var yesterday = new Date();
+// 	var beforeYesterday = new Date();
+// 	yesterday.setDate(now.getDate() - 1);
+// 	beforeYesterday.setDate(now.getDate() - 1);
+// 	return Topics.find({}, {
+// 		dateAdd: {
+// 			$lte: yesterday,
+// 			$gte: beforeYesterday
+// 		}
+// 	});
+// });
+
+// Meteor.publish("archiveTopics", function() {
+// 	var now = new Date();
+// 	var beforeYesterday = new Date();
+// 	beforeYesterday.setDate(now.getDate() - 2);
+// 	return Topics.find({}, {
+// 		dateAdd: {
+// 			$lte: beforeYesterday
+// 		}
+// 	});
+// });
+
+Meteor.publish('topics');
