00f8d2fa41e8e9798619b79a5f7d079a92bd59c3
Andrew Dacenko <a.s.dvornik@gmail.com>
2014-09-09 12:33:32 +0300

    Topic ctrl

--- static/js/main.js
+++ static/js/main.js
@@ -48,30 +48,58 @@
 		};
 
 		this.text = '';
+		this.to = null;
 
-		this.comment = function() {
+		this.submit = function() {
 			$http.post('/api/topic/' + self.id, {
 				text: this.text
 			}).then(function(response) {
-				self.topic.comments.push(response.data);
+				self.text = '';
+				self.to = null;
+				self.topic = response.data;
 			}, function(reason) {
 				console.log(reason);
 			});
-		}
+		};
 
+		this.comment = function($event, a) {
+			$event.preventDefault();
+
+			this.to = a.user;
+			this.text = a.user.username + ', ';
+
+			var form = angular.element('.comment-area');
+
+			form.focus();
+		};
 
 		$http.get('/api/topic/' + self.id).then(function(response) {
 			self.topic = angular.copy(response.data);
 		});
 	};
 
+	function nl2br($sce) {
+		return function(msg, is_xhtml) {
+			var is_xhtml = is_xhtml || true;
+			var breakTag = (is_xhtml) ? '<br />' : '<br>';
+			var msg = (msg + '').replace(/([^>\r\n]?)(\r\n|\n\r|\r|\n)/g, '$1' + breakTag + '$2');
+			return $sce.trustAsHtml(msg);
+		}
+	};
+
 	angular
 		.module('MyApp', ['angularMoment', 'ngCookies'])
-		.config(function($interpolateProvider) {
+		.config(function($httpProvider, $interpolateProvider) {
 			$interpolateProvider.startSymbol('{$');
 			$interpolateProvider.endSymbol('$}');
+			$httpProvider.defaults.transformRequest = function(data) {
+				if (data === undefined) return data;
+				return $.param(data);
+			};
+			$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
 		})
-		.controller('IndexController', ['$http', IndexController])
+	// .filter('nl2br', ['$sce', nl2br])
+	.controller('IndexController', ['$http', IndexController])
 		.controller('StateController', ['$http', '$location', StateController])
 		.controller('TopicController', ['$http', '$location', TopicController])
 		.run(function($http, $cookies) {
